b27367c58cd302861b46cd84c9b3fdb493cfcea9
euphoric-fire <alvaroeduardo3002@gmail.com>
2026-07-15 23:44:33 +0200

    adicionado fator de conexao DAO

---
+++ src/main/java/com/gestaostock/dao/ConexaoFactory.java
@@ -0,0 +1,22 @@
+package com.gestaostock.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/loja?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+    private static final String USUARIO = "root";
+    private static final String SENHA = "Alvaro3002";
+
+    public static Connection getConexao() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+        }
+    }
+
+}
