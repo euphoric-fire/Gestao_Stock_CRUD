ff50e3121c894fa613e55fd01af352b36677dad0
euphoric-fire <alvaroeduardo3002@gmail.com>
2026-07-15 23:55:08 +0200

    adicionada interface produtoDAO

---
+++ src/main/java/com/gestaostock/dao/ProdutoDAO.java
@@ -0,0 +1,18 @@
+package com.gestaostock.dao;
+
+import java.util.List;
+
+import com.gestaostock.model.Produto;
+
+public interface ProdutoDAO {
+    
+    void inserir(Produto Produto);
+
+    Produto buscarPorId(int id);
+
+    List<Produto> listarTodos();
+
+    void atualizar(Produto produto);
+
+    void deletar(int id);
+}
