2907f2ec024b9fd377a7e045d9aa07ba89821d6a
euphoric-fire <alvaroeduardo3002@gmail.com>
2026-07-16 00:41:07 +0200

    adicionados os metodos listarTodos Produtos e atualizar Produtos em ProdutoDAOImpl

--- src/main/java/com/gestaostock/dao/ProdutoDAOImpl.java
+++ src/main/java/com/gestaostock/dao/ProdutoDAOImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.gestaostock.model.Produto;
@@ -54,14 +55,48 @@ public class ProdutoDAOImpl implements ProdutoDAO {
 
     @Override
     public List<Produto> listarTodos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarTodos'");
+        String sql = "SELECT * FROM Produto";
+        List<Produto> produtos = new ArrayList<>();
+
+        try (Connection conn = ConexaoFactory.getConexao()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Produto produto = new Produto(rs.getInt("id"), rs.getString("nome"), rs.getDouble("preco"),
+                        rs.getInt("quatidade_estoque"));
+
+                produtos.add(produto);
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao listar produtos", e);
+        }
+
+        return produtos;
     }
 
     @Override
     public void atualizar(Produto produto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+        String sql = "UPDATE Produto SET nome = ', preco = ?, quantidade_estoque = ? WHERE id = ?";
+
+        try (Connection conn = ConexaoFactory.getConexao()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, produto.getNome());
+            ps.setDouble(2, produto.getPreco());
+            ps.setInt(3, produto.getQuantidadeEstoque());
+            ps.setInt(4, produto.getId());
+
+            int linhasAfetadas = ps.executeUpdate();
+            if (linhasAfetadas == 0) {
+                throw new RuntimeException("Nenhum produto foi encontrado com Id " + produto.getId());
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar produto", e);
+        }
     }
 
     @Override
